79e65682484366e8d6a12a549456eb8396a66e94
@hunk <ing.edgar@gmail.com>
2016-02-10 19:58:58 -0600

    update README

--- HKDebug.h
+++ HKDebug.h
@@ -8,46 +8,46 @@
 
 #import <Foundation/Foundation.h>
 
-#define XCODE_COLORS_ESCAPE @"\033["
-#define XCODE_COLORS_RESET_FG  XCODE_COLORS_ESCAPE @"fg;" // Clear any foreground color
-#define XCODE_COLORS_RESET_BG  XCODE_COLORS_ESCAPE @"bg;" // Clear any background color
-#define XCODE_COLORS_RESET     XCODE_COLORS_ESCAPE @";"
+#define HKDEBUG_ESCAPE          @"\033["
+#define HKDEBUG_RESET_FG        HKDEBUG_ESCAPE @"fg;" // Clear any foreground color
+#define HKDEBUG_RESET_BG HKDEBUG_ESCAPE @"bg;" // Clear any background color
+#define HKDEBUG_RESET    HKDEBUG_ESCAPE @";"
 
-#define XCODE_COLORS_SILVER  @"fg200,200,200;"
-#define XCODE_COLORS_BLUE    @"fg0,0,255;"
-#define XCODE_COLORS_GREEN   @"fg0,255,0;"
-#define XCODE_COLORS_YELLOW  @"fg255,255,0;"
-#define XCODE_COLORS_RED     @"fg255,0,0;"
-#define XCODE_COLORS_MAGENTA @"fg255,0,255;"
-#define XCODE_COLORS_CYAN    @"fg0,255,255;"
+#define HKDEBUG_SILVER  @"fg200,200,200;"
+#define HKDEBUG_BLUE    @"fg0,0,255;"
+#define HKDEBUG_GREEN   @"fg0,255,0;"
+#define HKDEBUG_YELLOW  @"fg255,255,0;"
+#define HKDEBUG_RED     @"fg255,0,0;"
+#define HKDEBUG_MAGENTA @"fg255,0,255;"
+#define HKDEBUG_CYAN    @"fg0,255,255;"
 
-#define XCODE_COLORS_VERBOSE @"%@.%@:%d " XCODE_COLORS_ESCAPE XCODE_COLORS_SILVER  @"VERBOSE"     XCODE_COLORS_RESET @": "
-#define XCODE_COLORS_DEBUG   @"%@.%@:%d " XCODE_COLORS_ESCAPE XCODE_COLORS_BLUE    @"DEBUG"       XCODE_COLORS_RESET @": "
-#define XCODE_COLORS_INFO    @"%@.%@:%d " XCODE_COLORS_ESCAPE XCODE_COLORS_GREEN   @"INFO"        XCODE_COLORS_RESET @": "
-#define XCODE_COLORS_WARNING @"%@.%@:%d " XCODE_COLORS_ESCAPE XCODE_COLORS_YELLOW  @"WARNING"     XCODE_COLORS_RESET @": "
-#define XCODE_COLORS_ERROR   @"%@.%@:%d " XCODE_COLORS_ESCAPE XCODE_COLORS_RED     @"ERROR"       XCODE_COLORS_RESET @": "
-#define XCODE_COLORS_ALT     @"%@.%@:%d " XCODE_COLORS_ESCAPE XCODE_COLORS_MAGENTA @"ALTERNATIVE" XCODE_COLORS_RESET @": "
-#define XCODE_COLORS_OPT     @"%@.%@:%d " XCODE_COLORS_ESCAPE XCODE_COLORS_CYAN    @"OPTIONAL"    XCODE_COLORS_RESET @": "
+#define HKDEBUG_VERBOSE @"%@.%@:%d " HKDEBUG_ESCAPE HKDEBUG_SILVER  @"VERBOSE"     HKDEBUG_RESET @": "
+#define HKDEBUG_DEBUG   @"%@.%@:%d " HKDEBUG_ESCAPE HKDEBUG_BLUE    @"DEBUG"       HKDEBUG_RESET @": "
+#define HKDEBUG_INFO    @"%@.%@:%d " HKDEBUG_ESCAPE HKDEBUG_GREEN   @"INFO"        HKDEBUG_RESET @": "
+#define HKDEBUG_WARNING @"%@.%@:%d " HKDEBUG_ESCAPE HKDEBUG_YELLOW  @"WARNING"     HKDEBUG_RESET @": "
+#define HKDEBUG_ERROR   @"%@.%@:%d " HKDEBUG_ESCAPE HKDEBUG_RED     @"ERROR"       HKDEBUG_RESET @": "
+#define HKDEBUG_ALT     @"%@.%@:%d " HKDEBUG_ESCAPE HKDEBUG_MAGENTA @"ALTERNATIVE" HKDEBUG_RESET @": "
+#define HKDEBUG_OPT     @"%@.%@:%d " HKDEBUG_ESCAPE HKDEBUG_CYAN    @"OPTIONAL"    HKDEBUG_RESET @": "
 //#define XCODE_COLORS_MORE    @"%@:%d "
 
-#define XCODE_FILENAME [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] componentsSeparatedByString:@"."] firstObject]
+#define HKDEBUG_FILENAME [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] componentsSeparatedByString:@"."] firstObject]
 
 #ifdef DEBUG
-#define HVerbose(frmt, ...) hunkCustomLog((XCODE_COLORS_VERBOSE frmt),[XCODE_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
-#define HDebug(frmt, ...)   hunkCustomLog((XCODE_COLORS_DEBUG frmt),  [XCODE_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
-#define HInfo(frmt, ...)    hunkCustomLog((XCODE_COLORS_INFO frmt),   [XCODE_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
-#define HWarning(frmt, ...) hunkCustomLog((XCODE_COLORS_WARNING frmt),[XCODE_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
-#define HError(frmt, ...)   hunkCustomLog((XCODE_COLORS_ERROR frmt),  [XCODE_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
-#define HAlt(frmt, ...)     hunkCustomLog((XCODE_COLORS_ALT frmt), [XCODE_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
-#define HOpt(frmt, ...)     hunkCustomLog((XCODE_COLORS_OPT frmt),[XCODE_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
+#define HKVerbose(frmt, ...) hunkCustomLog( (HKDEBUG_VERBOSE frmt),[HKDEBUG_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
+#define HKDebug(frmt, ...)   hunkCustomLog( (HKDEBUG_DEBUG frmt),  [HKDEBUG_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
+#define HKInfo(frmt, ...)    hunkCustomLog( (HKDEBUG_INFO frmt),   [HKDEBUG_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
+#define HKWarning(frmt, ...) hunkCustomLog( (HKDEBUG_WARNING frmt),[HKDEBUG_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
+#define HKError(frmt, ...)   hunkCustomLog( (HKDEBUG_ERROR frmt),  [HKDEBUG_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
+#define HKAlt(frmt, ...)     hunkCustomLog( (HKDEBUG_ALT frmt),    [HKDEBUG_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
+#define HKOpt(frmt, ...)     hunkCustomLog( (HKDEBUG_OPT frmt),    [HKDEBUG_FILENAME, NSStringFromSelector(_cmd), __LINE__, ##__VA_ARGS__)
 #else
-#define HVerbose
-#define HDebug
-#define HInfo
-#define HWarning
-#define HError
-#define HAlt
-#define HOpt
+#define HKVerbose
+#define HKDebug
+#define HKInfo
+#define HKWarning
+#define HKError
+#define HKAlt
+#define HKOpt
 #endif
 
 //#define NSLog(frmt, ...) hunkCustomLog((XCODE_MORE frmt),[[NSString stringWithUTF8String:__FILE__] lastPathComponent],__LINE__, ##__VA_ARGS__)
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # HKDebug
-Another custom Debug with Colors (based in SwiftyBeaver but for Objective-C)
+Another custom Debug with Colors (based in [SwiftyBeaver](https://github.com/SwiftyBeaver/SwiftyBeaver) but for Objective-C) with help of [XcodeColors](https://github.com/robbiehanson/XcodeColors)
+
+I love [SwiftyBeaver](https://github.com/SwiftyBeaver/SwiftyBeaver), it is a realy good. but i have some projects in Objective-C and i want the same output for the Xcode console. [SwiftyBeaver](https://github.com/SwiftyBeaver/SwiftyBeaver) have a wrapper [SBObjectiveCWrapper](https://github.com/SwiftyBeaver/SBObjectiveCWrapper) for Objective-C but you need a AppDelegate in Swift and my old proyects are all in Objective-C 
+
+So, i decided to make this little class (function and macros) for have the same output.
+<img src="https://raw.githubusercontent.com/hunk/MediaDemo/master/HKDebug/console.png">
+
+##Installation
+
+###Download
+1. Download the [project](https://github.com/hunk/HKDebug/archive/master.zip)
+2. Drag & drop the `HKDebug.[h|m]` files into your project (make sure "Copy items if needed" is checked)
+
+###Usage
+
+Add `#import "HKDebug.h"` in your files where you need or if you use `.pch` add here and uses in all your files.
+
+Now you can use the follow functions: 
+
+```objective-c
+    HKVerbose("not so important");  // prio 1, VERBOSE in silver
+    HKDebug("something to debug");  // prio 2, DEBUG in blue
+    HKInfo("a nice information");   // prio 3, INFO in green
+    HKWarning("oh no, that won’t be good");  // prio 4, WARNING in yellow
+    HKError("ouch, an error did occur!"); // prio 5, ERROR in red
+    HKAlt(@"alternative color"); // prio 5, ALTERNATIVE in magenta
+    HKOpt(@"optional color"); // prio 5, OPTIONAL in cyan
+    
+    // log anything!
+    HKVerbose(@"%zd",123);
+    HKInfo(@"%zd",-123.45678);
+    HKWarning(@"%@",[NSDate date]);
+    HKError(@"%@",@[@"I", @"like", @"logs!"]);
+    HKError(@"%@",@{@"name": @"Mr Bean", @"address": @"7 Some Street"});
+```
+### Don't show colors?
+Check if you have installed [XcodeColors](https://github.com/robbiehanson/XcodeColors)
+
+-  Testing to see if XcodeColors is installed and enabled:
+
+	```objective-c
+	char *xcode_colors = getenv("XcodeColors");
+	if (xcode_colors && (strcmp(xcode_colors, "YES") == 0))
+	{
+   	// XcodeColors is installed and enabled!
+	}
+	```
+
+
+## License
+
+HKDebug is released under the [MIT License](https://github.com/hunk/HKDebug/blob/master/LICENSE).
